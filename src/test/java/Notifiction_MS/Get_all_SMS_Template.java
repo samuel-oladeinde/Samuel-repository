a61e733cf5a3a82e2eb9fe37ccb0988fe01b5ef6
`Eniola <eiyaomolere@reliancehmo.com>
2022-07-24 10:46:56 +0100

    Testing

--- src/test/java/Notifiction_MS/Get_all_SMS_Template.java
+++ src/test/java/Notifiction_MS/Get_all_SMS_Template.java
@@ -16,12 +16,22 @@ public class Get_all_SMS_Template extends TestBase {
 
     @Test(priority = 1)
     @Severity(SeverityLevel.CRITICAL)
-    @Description("An Endpoint to Get User By Latest Message not Enrolled 200")
-    @Story("An Endpoint to Get User By Latest Message not Enrolled 200")
-    public void GetUser_By_Latest_Message_Not_Enrolled_200() throws InterruptedException, IOException {
+    @Description("An Endpoint to Get all SMS template when a valid authorization is provided")
+    @Story("Ensure that a user is able to get all SMS Template")
+    public void Get_All_SMS_Template() throws InterruptedException, IOException {
         Response response = GET_200(Get_All_sms_template_EndPoint);
         response.prettyPeek().then().spec(responseSpec_200);
-        response.then().assertThat().body("content[0]", is(59));
-        response.then().assertThat().body("name[0]", is(59));
+        response.then().assertThat().body("content[0]", is(" Welcome to bamti {firstname} {LastName}"));
+        response.then().assertThat().body("name[0]", is("welcome you all"));
+        response.then().assertThat().body("id[0]", is(5));
+    }
+
+    @Test(priority = 2)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("An Endpoint to Get all SMS template when a valid authorization is provided")
+    @Story("Ensure that a user is able to get all SMS Template")
+    public void Get_All_SMS_Template_Invalid_Authorization() throws InterruptedException, IOException {
+        Response response = GET_401(Get_All_sms_template_EndPoint);
+        response.prettyPeek().then().spec(responseSpec_403);
     }
 }
